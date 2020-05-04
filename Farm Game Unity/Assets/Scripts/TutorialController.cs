72ddb6dfb0e1f1a9a10f20d6b8b6ed42bc89fe41
Ernesto Uson <erni.uson@gmail.com>
2020-05-04 17:08:47 +0200

    De momento el tutorial asi esta guay para hacer una build

--- Farm Game Unity/Assets/Scripts/TutorialController.cs
+++ Farm Game Unity/Assets/Scripts/TutorialController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TutorialController : MonoBehaviour
 {
     private int state = -1;
-
+    public GameObject popUp;
     public static TutorialController instance;
     private void Awake()
     {
@@ -18,12 +18,24 @@ public class TutorialController : MonoBehaviour
         UpdateStates();
     }
 
+    public void TutorialPopUp()
+    {
+        popUp.SetActive(true);
+        InputManager.instance.ChangeState(InputManager.States.OnUI);
+    }
+
+    public void ClosePopUp()
+    {
+        popUp.SetActive(false);
+        InputManager.instance.ChangeState(InputManager.States.Idle);
+    }
+
     private void UpdateStates()
     {
         switch (state)
         {
             case 0:
-                NpcManager.instance.StartForcedDialogue("Maria", "Hi! I'm Maria. In the mailbox next to me you will find your first order!\nCome back to me when you have finished");
+                NpcManager.instance.StartForcedDialogue("Maria", "Hi! I'm Maria. In the mailbox next to me you will find your first order!\nCome back to me when you're done");
                 break;
             case 1:
                 break;
