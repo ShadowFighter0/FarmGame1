9dfcb8637a4bb8a97748e761897382475ec1cbf8
Ernesto Uson <erni.uson@gmail.com>
2020-05-08 22:19:43 +0200

    esto va a doler

--- Farm Game Unity/Assets/Scripts/NPCs/MailBoxController.cs
+++ Farm Game Unity/Assets/Scripts/NPCs/MailBoxController.cs
@@ -15,16 +15,31 @@ public class MailBoxController : MonoBehaviour
 
     private bool done = true;
 
+    private float timer;
+    public float time;
+
+    private int mailIndex = 0;
+    public GameObject mailImage;
+
     private void Start()
     {
         mailsPanel = mailFolder.parent.gameObject;
 
         mails = Resources.LoadAll<Mail>("Data/Mails");
 
-        AddContent(mails[0]);
+        AddContent(mails[mailIndex]);
     }
     private void Update()
     {
+        float dt = Time.deltaTime;
+        timer += dt;
+        if (timer > time && mailIndex < mails.Length - 1)
+        {
+            timer = 0;
+            mailIndex++;
+            AddContent(mails[mailIndex]);
+        }
+
         if (playerNear)
         {
             if (Input.GetKeyDown(InputManager.instance.Interact) && done)
@@ -40,7 +55,12 @@ public class MailBoxController : MonoBehaviour
             }
         }
     }
+    private IEnumerator CloseUIMail()
+    {
+        yield return new WaitForSeconds(3);
+        mailImage.SetActive(false);
 
+    }
     public void Close()
     {
         mailsPanel.SetActive(false);
@@ -73,6 +93,9 @@ public class MailBoxController : MonoBehaviour
             {
                 child.gameObject.SetActive(true);
                 child.GetComponent<MailBoxPanel>().Assign(info);
+
+                mailImage.SetActive(true);
+                StartCoroutine(CloseUIMail());
                 return;
             }
         }
@@ -85,6 +108,9 @@ public class MailBoxController : MonoBehaviour
             {
                 child.gameObject.SetActive(true);
                 child.GetComponent<MailBoxPanel>().Assign(info);
+
+                mailImage.SetActive(true);
+                StartCoroutine(CloseUIMail());
                 return;
             }
         }
@@ -94,6 +120,7 @@ public class MailBoxController : MonoBehaviour
     {
         int i = GetActiveMails() - 1;
         mailFolder.GetChild(i).GetComponent<MailBoxPanel>().AddContent();
+
     }
 
     private void OnTriggerEnter(Collider other)
