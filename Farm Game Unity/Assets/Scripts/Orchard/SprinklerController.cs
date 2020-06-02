202fab3c2b1dcdc4b67292243768c4c138a8a0ac
Ernesto Uson <erni.uson@gmail.com>
2020-06-02 18:34:05 +0200

    dale carla

--- Farm Game Unity/Assets/Scripts/Orchard/SprinklerController.cs
+++ Farm Game Unity/Assets/Scripts/Orchard/SprinklerController.cs
@@ -6,6 +6,10 @@ public class SprinklerController : MonoBehaviour
 {
     private Transform leftChild;
     private Transform rightChild;
+
+    private Transform[] leftChildPivots;
+    private Transform[] rightChildPivots;
+
     private GameObject leftWater;
     private GameObject rightWater;
     private void Awake()
@@ -17,17 +21,60 @@ public class SprinklerController : MonoBehaviour
         rightChild = transform.GetChild(1);
         rightWater = rightChild.GetChild(0).gameObject;
         rightWater.SetActive(false);
+
+        leftChildPivots = new Transform[leftChild.childCount];
+        for (int i = 0; i < leftChildPivots.Length; i++)
+        {
+            leftChildPivots[i] = leftChild.GetChild(i);
+        }
+
+        rightChildPivots = new Transform[rightChild.childCount];
+        for (int i = 0; i < leftChildPivots.Length; i++)
+        {
+            rightChildPivots[i] = rightChild.GetChild(i);
+        }
     }
     private void OnEnable()
     {
         rightWater.SetActive(true);
         leftWater.SetActive(true);
     }
-    // Update is called once per frame
+
     void Update()
     {
         Vector3 rot = Vector3.up * Time.deltaTime * 30;
         leftChild.Rotate(rot);
         rightChild.Rotate(-rot);
+
+        for (int i = 0; i < leftChildPivots.Length; i++)
+        {
+            Ray ray = new Ray(leftChildPivots[i].position, Vector3.down);
+
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if(hit.transform.gameObject.CompareTag("Hole"))
+                {
+                    hit.transform.GetComponent<HoleController>().AddWater(100.0f);
+                }
+            }
+        }
+
+        for (int i = 0; i < rightChildPivots.Length; i++)
+        {
+            Ray ray = new Ray(rightChildPivots[i].position, Vector3.down);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform.gameObject.CompareTag("Hole"))
+                {
+                    hit.transform.GetComponent<HoleController>().AddWater(100.0f);
+                }
+            }
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject);
     }
 }
