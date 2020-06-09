9ab8f8979d4184bbad64a185eda017d37604cde9
Ernesto Uson <erni.uson@gmail.com>
2020-06-10 01:24:00 +0200

    sdfsdfdsfsd

---
+++ Farm Game Unity/Assets/Scripts/UI/MenuButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButtonController : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    private AudioClip clickSound;
+    private AudioClip overSound;
+    private void Start() {
+        clickSound = DataBase.GetAudioClip("Pop");
+        overSound = DataBase.GetAudioClip("Button");
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AudioManager.PlaySoundWithVariation(overSound);
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioManager.PlaySoundWithVariation(clickSound);
+    }
+}
